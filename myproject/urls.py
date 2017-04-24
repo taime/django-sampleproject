599735eb286ec15afa5fca51152284c29ad131e8
tema <regibox@icloud.com>
2017-04-24 12:42:39 +0300

    Add ALLAUTH  	modified:   db.sqlite3  	modified:   myproject/__pycache__/settings.cpython-35.pyc  	modified:   myproject/__pycache__/urls.cpython-35.pyc  	modified:   myproject/settings.py  	modified:   myproject/urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^account/', include('registration.backends.default.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('allauth.urls')),
 ]

97592e9d2eac5a8cfe9437af6c0c4f13bc8c3cdb
tema <regibox@icloud.com>
2017-04-24 10:31:12 +0300

    Trying add crispy forms

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'crispy_forms',
+    'bootstrapform',
     'mysite'
 ]
 
@@ -128,3 +130,5 @@ REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged
 LOGIN_REDIRECT_URL = '/'  # The page you want users to arrive at after they successful log in
 LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
                                                                 # and are trying to access pages requiring authentication
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap3'
